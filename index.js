af69aede08c07390e0f72891388a96aa7c625f5f
Stephanie Simpson <stephsimpson911@gmail.com>
2018-02-13 19:40:33 +0000

    recursive arrays!

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+    // Print out all it's items individually
+  let  arraySum = 0;
+  array.forEach((number) => {
+      if (Array.isArray(number)) {
+        // Print out all it's items individually
+        arraySum += sumItems(number)
+      } else {
+      arraySum += number;
+    } 
+  });
+  return arraySum;
 }
 
 module.exports = sumItems;
